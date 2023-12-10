adaefa1ca381ee6cf9341eb39cf9af7924d6e268
taeyeon <hahaha2662@naver.com>
2023-12-10 18:13:41 +0900

    last

--- app/courses/page.jsx
+++ app/courses/page.jsx
@@ -2,7 +2,7 @@
 import Courses from '@/app/components/courses/Courses'
 const CoursesPage = () => {
   return (
-    <div className="text-center">
+    <div className="text-center" data-aos="fade-up">
        <h1 className="text-4xl font-bold mb-6">My Project</h1>
       <Courses />
     </div>
